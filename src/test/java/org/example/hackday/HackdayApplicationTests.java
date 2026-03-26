1550dd1bcd8be9b21b3d1ade15bdbcbbcda03ec9
MaXiM4720 <maxim47xxx@gmail.com>
2026-03-26 17:45:04 +0100

    1

---
+++ src/test/java/org/example/hackday/HackdayApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.hackday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HackdayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
